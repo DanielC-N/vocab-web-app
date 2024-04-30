625e605776915ee42513a530373c604f4c84daea
lmeline <loisemel5@gmail.com>
2024-04-30 15:48:58 +0200

    ajout d'elements dans la table

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*$servername ="localhost";
+$database="schema.sql"; 
+$username= "root";
+$password= "formation";
+
+$connexion = mysqli_connect($servername, $username, $password, $database);
+
+if (!$connexion) {
+    die("connexion failed:" .mysqli_connect_error());
+}
+echo "connexion successfully";
+mysqli_connect($connexion);
+
+?>*/
---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS traduction;
+
+USE traduction;
+
+CREATE TABLE vocabulaire (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    mot_fr TEXT NOT NULL,
+    mot_en TEXT NOT NULL,
+    note TEXT,
+    modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+INSERT INTO vocabulaire (id,mot_fr,mot_en,note,modified,created -u) VALUES
+("pharisiens", "pharisees"),
+("juifs","Jews"),
+("Flavius Josèphe","Flavius Josephus"),
+("Gethsémané","Gethsemane")
+
+
+
+INSERT INTO vocabulaire(modified,created)
+
+
