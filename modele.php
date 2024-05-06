4396bd427969e0e15705486c66a9b475cb1c7a5f
lmeline <loisemel5@gmail.com>
2024-05-06 17:31:26 +0200

    ajout de commentaire dans le modele.php

--- modele.php
+++ modele.php
@@ -5,9 +5,27 @@ function getBaseDD(){
     $resultats = $bdd->query('SELECT * FROM vocabulaire ORDER BY created DESC; ');
 
     return $resultats;
-//delete
-//get 
-//add
 }
-
 ?>
+<!--delete
+DELETE FROM vocabulaire WHERE id= nombre;
+
+$connexion = mysql_connect()
+*/
+//get 
+UPDATE vocabulaire SET mot_fr =:mot_fr, mot_en=:mot_en, note=:note, vocabulaire=:vocabulaire WHERE vocabualaire_id=:id 
+
+
+/* add 
+try {
+    $resultats =getBaseDD();
+}
+catch (Exception $e) {
+    die ('erreur : ' . $e->getMessage());
+}
+$sqlQuery = 'INSERT INTO recipes(mot_fr,mot_en,note) VALUES (:mot_fr,:mot_en,:note)';
+
+$insertBaseDD = $resultats-> prepare($sqlQuery);
+
+$insertBaseDD->
+-->
